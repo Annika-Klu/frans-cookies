119e9a41bcdd18312f4a73b52e4470e5e5cc4d69
Annika Klüpfel <Annika.Kluepfel@ils.de>
2026-06-18 16:04:05 +0200

    configure sainty studio in cms dir

---
+++ cms/schemaTypes/cookie.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "cookie",
+  title: "Cookie",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+    }),
+  ],
+});
